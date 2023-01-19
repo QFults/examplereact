40a641829335dd7b12c7367d55d35097598e678c
Quinton Fults <quinton.fults.work@gmail.com>
2023-01-19 13:40:11 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Greeting from "./components/Greeting";
 
 // const name = "John Doe";
 
-const names = ["John Doe", "Jane Doe", "Jack Doe", "Jerry Doe", "Jenna Doe"];
+// const names = ["John Doe", "Jane Doe", "Jack Doe", "Jerry Doe", "Jenna Doe"];
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleBtnClick = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div className="App">
-      {names.map((name, i) => (
+      <h1>Count: {count}</h1>
+      <button onClick={handleBtnClick}>Click Me</button>
+
+      {/* {names.map((name, i) => (
         <Greeting key={i} name={name} />
-      ))}
+      ))} */}
       {/* <Greeting name={name} />
       <Greeting name="Jane Doe" /> */}
       {/* {[<button>a</button>, <button>b</button>, <button>c</button>]} */}
