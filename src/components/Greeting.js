bf4665a4753be24f862239cf29680d4d84f86c75
Quinton Fults <quinton.fults.work@gmail.com>
2023-01-19 11:19:26 -0800

    code

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,17 @@
+const name = 'John Doe'
+const a = 4
+const b = 5
+
 function Greeting() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello {name}!</h1>
+      <h1>Goodbye World!</h1>
+      <p>Sum: {a + b}</p>
+      <p>{Math.floor(Math.random() * 100) + 1}</p>
+      {(() => { return 5 })()}
+    </>
+  );
 }
 
 export default Greeting;
