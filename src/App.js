b895ed19edb5bb0e71be7f6daafa17525b870690
Quinton Fults <quinton.fults.work@gmail.com>
2023-01-19 10:51:31 -0800

    code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Greeting from "./components/Greeting";
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting />
+      <button>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
