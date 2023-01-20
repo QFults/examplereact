c320ca9da05633cca51b12c585f07d74e20366be
Quinton Fults <quinton.fults.work@gmail.com>
2023-01-20 11:55:08 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import { useEffect } from "react";
 import searchOMDB from "./utils/OMDB";
+// import './App.css'
+
+const styles = {
+  heading: {
+    color: 'red',
+    backgroundColor: 'blue'
+  }
+}
 
 function App() {
 
@@ -12,7 +20,7 @@ function App() {
     searchMovie('Goodfellas')
   }, [])
 
-  return <div></div>;
+  return <h1 style={styles.heading}>Hello World!</h1>;
 }
 
 export default App;
